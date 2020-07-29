99e8c8eaf2d79509e28e65607fdffc32fb3db5fd
ulfat-dawood <ulfat_dawood@outlook.com>
2020-07-30 02:45:50 +0530

    setting up the env

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,15 @@
-import react from 'react'; 
+import React from 'react'; 
 import logo from './logo.svg';
 import './App.css'; 
 
  //Functional based components: 
+
+ function App(){
+   return(
+     <h1>
+        ho
+     </h1>
+   );
+ }
+
+ export default App; 
