49374d4a6eb9b1ea6f1cc9a1c332d65ff8dfd9b4
ulfat-dawood <ulfat_dawood@outlook.com>
2020-07-30 06:35:15 +0530

    functional based component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react'; 
 import logo from './logo.svg';
 import './App.css'; 
+import { render } from 'react-dom';
 
  //Functional based components: 
 
- function App(){
-   return(
-     <h1>
-        ho
-     </h1>
-   );
- }
+//  function App(){
+//    return(
+//      <h1>
+//         ho
+//      </h1>
+//    );
+//  }
 
- export default App; 
+//  export default App; 
+
+
+//Class based components: 
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return(
+      <h1>hi</h1>
+    )
+  }
+}
