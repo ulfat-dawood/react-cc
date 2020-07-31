4d9ba216a196fce37de49800bd72d248920800c6
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-01 05:04:29 +0530

    orogonal files to be deleted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,14 +8,26 @@ class App extends React.Component{
 
   render(){
     return(
-      <div>
+      <div style={{maxWidth:"700px", margin:"10px auto"}}> 
         <h1 className="app-title">ToDo App</h1>
          <div className="container">
            Add an item... 
          </div>
          <br/>
          <input type="text" className="input-text" placeholder="write a todo"/>
+         <button className="add-btn">Add to-do</button>
+         <div className="list">
+           <ul>
+             <li>
+               <input type="checkbox" name="" id=""/>
+               Read a book
+               <button class="btn">Delete</button>
+             </li>
+           </ul>
+         </div>
       </div>
     )
   }
 }
+
+export default App; 
