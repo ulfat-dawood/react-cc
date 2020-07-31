7c9912293b699f117e57f6909851ee7b49f12fc6
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-01 04:40:02 +0530

    react class based component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,45 +3,18 @@ import logo from './logo.svg';
 import './App.css'; 
 import { render } from 'react-dom';
 
- //Functional based components: 
-
-//  function App(){
-//    return(
-//      <h1>
-//         ho
-//      </h1>
-//    );
-//  }
-
-//  export default App; 
-
-
 //Class based components: 
 class App extends React.Component{
-
-  constructor(props){
-    super(props);
-    this.state= {
-      name:'',
-      balance: 0
-    }
-  }
-
-  handleChange(name){
-    this.setState({
-      name: name
-    });
-  }
 
   render(){
     return(
       <div>
-        <input 
-        type="text" 
-        value={this.state.name} 
-        onChange={e=> this.handleChange(e.target.value)}>
-        
-        </input>
+        <h1 className="app-title">ToDo App</h1>
+         <div className="container">
+           Add an item... 
+         </div>
+         <br/>
+         <input type="text" className="input-text" placeholder="write a todo"/>
       </div>
     )
   }
