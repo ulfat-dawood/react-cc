850b7b4f32f68c7e42ee5de82ce9933565a85497
ulfat-dawood <ulfat_dawood@outlook.com>
2020-08-01 07:45:53 +0530

    FULL CLASS COMPONENT

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,85 +1,35 @@
 import React from 'react'; 
-import logo from './logo.svg';
-import './App.css'; 
-import { render } from 'react-dom';
+//çimport './App.css'; 
+
 
 //Class based components: 
-class App extends React.Component{
+export default class App extends React.Component{
 
   constructor(props){
     super(props);
     this.state={
-      newItem: {},
-      list:[]
+      text: ''
     }
   }
 
-  addItem(todoValue){
-    if(todoValue !==""){
-       const newItem={
-         id: Date.now(),
-         value: todoValue,
-         isDone: false 
-       }
-       const list=[...this.state.list]
-       list.push(newItem);  
-       this.setState({
-         list, 
-         newItem: {}
-       }); 
-    }
-  }
+ handleChange(text){
+   this.setState({
+     text:text
+   })
+ }
 
-  deleteItem(id){
-    const list= [...this.state.list] 
-    const updatedList=  list.filter((item)=>item.id !== id);
-    this.setState({list: updatedList}); 
-  }
-
-  updateInput(input){
-    this.setState({newItem: input}); 
-  }
-
-  handleChecked(id){
-    
-    const list= [...this.state.list]  
-    list.map(item=> {item.id=id?item.isDone=!item.isDone:null})
-    this.setState({list}); 
-  }
+ handleSubmit(){
+  alert(this.state.text); 
+ }
 
   render(){
     return(
       <div style={{maxWidth:"700px", margin:"10px auto"}}> 
-        <h1 className="app-title">ToDo App</h1>
-         <div className="container">
-           Add an item... 
-         </div>
-         <br/>
-         <input type="text" className="input-text" placeholder="write a todo"
-         value={this.state.newItem.value} onChange={e => this.updateInput(e.target.value)}/>
-         <button className="add-btn"
-         onClick={()=>this.addItem(this.state.newItem)}
-         disabled={!this.state.newItem.length}
-         >Add to-do</button>
-         <div className="list">
-           <ul>
-             {this.state.list.map(item=>{
-               return(
-                 <li key={item.id}>
-                  <input type="checkbox" checked={item.isDone} onChange={()=>this.handleChecked(item.id)}/>
-                  {item.value }
-                  <button class="btn"
-                  onClick={()=>this.deleteItem(item.id)}
-                  >Delete</button>
-                 </li>
-               )
-             })}
-            
-           </ul>
-         </div>
+        <input type="text" value={this.state.text} onChange={e=>this.handleChange(e.target.value)}/>
+        <button onClick={()=>this.handleSubmit()}>Submit</button>
       </div>
     )
   }
 }
 
-export default App; 
+//export default App; 
